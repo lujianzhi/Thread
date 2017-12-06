e5dbf5b8facce82a946265e9bf2bb8bf508b1d16
lujianzhi <lujianzhixiaoyao@163.com>
2017-12-06 23:00:01 +0800

    Java多线程系列--“基础篇”01之 基本概念 Java多线程系列--“基础篇”02之 常用的实现多线程的两种方式 Java多线程系列--“基础篇”03之 Thread中start()和run()的区别

---
+++ src/skywang/chapter2/ThreadC2.java
@@ -0,0 +1,17 @@
+package skywang.chapter2;
+
+/**
+ * Created by Ian.Lu on 2017/12/6.
+ * Project : Thread
+ */
+public class ThreadC2 extends Thread {
+
+    private int tickets = 10;
+
+    @Override
+    public void run() {
+        super.run();
+        while (tickets > 0)
+            System.out.println(tickets--);
+    }
+}
---
+++ src/skywang/chapter3/Test.java
@@ -0,0 +1,17 @@
+package skywang.chapter3;
+
+/**
+ * Created by Ian.Lu on 2017/12/6.
+ * Project : Thread
+ */
+public class Test {
+    public static void main(String[] args) {
+        ThreadC3 threadC3 = new ThreadC3("Ian");
+
+        System.out.println(Thread.currentThread().getName() + "启动线程");
+        threadC3.run();
+
+        System.out.println(Thread.currentThread().getName() + "启动线程");
+        threadC3.start();
+    }
+}
