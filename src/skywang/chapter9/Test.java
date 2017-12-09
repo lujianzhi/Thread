d3b996d1679aa59dde4a0d08ca7e4a4f7e8bad93
lujianzhi <lujianzhixiaoyao@163.com>
2017-12-09 17:56:19 +0800

    Java多线程系列--“基础篇”09之 interrupt()和线程终止方式

---
+++ src/skywang/chapter9/Test.java
@@ -0,0 +1,64 @@
+package skywang.chapter9;
+
+import skywang.chapter9.item3.ThreadC9Item3Blocked;
+import skywang.chapter9.item3.ThreadC9Item3ExtraParam;
+
+/**
+ * Created by Ian.Lu on 2017/12/9.
+ * Project : Thread
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        item3_1();
+//        item3_2();
+    }
+
+    /**
+     * 下面是通过“额外添加标记”的方式终止“状态状态”的线程的示例
+     */
+    private static void item3_2() {
+
+        try {
+            ThreadC9Item3ExtraParam threadC9Item3ExtraParam = new ThreadC9Item3ExtraParam("ThreadC9Item3ExtraParam");
+            System.out.println(threadC9Item3ExtraParam.getName() + "的状态 : " + threadC9Item3ExtraParam.getState().name());
+
+            threadC9Item3ExtraParam.start();
+            System.out.println(threadC9Item3ExtraParam.getName() + "的状态 : " + threadC9Item3ExtraParam.getState().name());
+
+            Thread.sleep(300);
+            threadC9Item3ExtraParam.stopThread();
+            System.out.println(threadC9Item3ExtraParam.getName() + "stopThread()后的状态 : " + threadC9Item3ExtraParam.getState().name());
+
+            Thread.sleep(300);
+            System.out.println(threadC9Item3ExtraParam.getName() + "的状态 : " + threadC9Item3ExtraParam.getState().name());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * interrupt()常常被用来终止“阻塞状态”线程。
+     */
+    private static void item3_1() {
+        try {
+            ThreadC9Item3Blocked threadC9Item3Blocked = new ThreadC9Item3Blocked("ThreadC9Item3Blocked");
+            System.out.println(threadC9Item3Blocked.getName() + "的状态 : " + threadC9Item3Blocked.getState().name());
+
+            threadC9Item3Blocked.start();
+            System.out.println(threadC9Item3Blocked.getName() + "的状态 : " + threadC9Item3Blocked.getState().name());
+
+            Thread.sleep(300);
+            System.out.println(threadC9Item3Blocked.getName() + "stopThread()前isInterrupted : " + threadC9Item3Blocked.isInterrupted());
+            threadC9Item3Blocked.interrupt();
+            System.out.println(threadC9Item3Blocked.getName() + "stopThread()后isInterrupted : " + threadC9Item3Blocked.isInterrupted());
+            System.out.println(threadC9Item3Blocked.getName() + "调用了interrupt()后的状态 : " + threadC9Item3Blocked.getState().name());
+
+            Thread.sleep(300);
+            System.out.println(threadC9Item3Blocked.getName() + "的状态 : " + threadC9Item3Blocked.getState().name());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/skywang/chapter9/item3/ThreadC9Item3Blocked.java
@@ -0,0 +1,32 @@
+package skywang.chapter9.item3;
+
+/**
+ * Created by Ian.Lu on 2017/12/9.
+ * Project : Thread
+ */
+public class ThreadC9Item3Blocked extends Thread {
+
+    public ThreadC9Item3Blocked(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            int i = 0;
+            System.out.println(Thread.currentThread().getName() + " 进入while循环前 : isInterrupted : " + isInterrupted());
+            while (!isInterrupted()) {
+                System.out.println(Thread.currentThread().getName() + " sleep之前 : isInterrupted : " + isInterrupted());
+                //线程会处于休眠阻塞状态
+                sleep(100);
+                i++;
+                System.out.println(Thread.currentThread().getName() + "的状态 : " + Thread.currentThread().getState().name() + " ; i : " + i);
+                System.out.println(Thread.currentThread().getName() + " sleep之后 : isInterrupted : " + isInterrupted());
+            }
+        } catch (InterruptedException e) {
+            System.out.println(Thread.currentThread().getName() + "的状态 : " + Thread.currentThread().getState().name() + " ; 抛出InterruptedException异常");
+            System.out.println(Thread.currentThread().getName() + " 发生异常 : isInterrupted : " + isInterrupted());
+        }
+    }
+}
