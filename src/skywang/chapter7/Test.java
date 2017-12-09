5f6e6e5f75a1f07f549f013a7de765e954b1840a
lujianzhi <lujianzhixiaoyao@163.com>
2017-12-09 13:40:36 +0800

    Java多线程系列--“基础篇”07之 线程休眠

---
+++ src/skywang/chapter7/Test.java
@@ -0,0 +1,20 @@
+package skywang.chapter7;
+
+import skywang.chapter7.item3.ThreadC7Item3;
+
+/**
+ * Created by Ian.Lu on 2017/12/9.
+ * Project : Thread
+ */
+public class Test {
+    public static void main(String[] args) {
+        item3();
+    }
+
+    private static void item3() {
+        ThreadC7Item3 threadC7Item3_1 = new ThreadC7Item3("ThreadC7Item3_1");
+        ThreadC7Item3 threadC7Item3_2 = new ThreadC7Item3("ThreadC7Item3_2");
+        threadC7Item3_1.start();
+        threadC7Item3_2.start();
+    }
+}
